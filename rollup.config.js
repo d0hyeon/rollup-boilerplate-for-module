5aaf3230e457c5e94a331e995729140bac708043
d0hyeon <ehgus6887@gmail.com>
2021-04-12 23:55:20 +0900

    fix: rollup import 버그 및 설정 수정

--- rollup.config.js
+++ rollup.config.js
@@ -1,30 +1,32 @@
-const babel = require('@rollup/plugin-babel');
-const resolve = require('@rollup/plugin-node-resolve');
+import babel from '@rollup/plugin-babel';
+import gzipPlugin from 'rollup-plugin-gzip';
+
 const commonjs = require('@rollup/plugin-commonjs');
 const typescript = require('@rollup/plugin-typescript');
-const gzipPlugin = require('rollup-plugin-gzip');
-
+const { nodeResolve } = require('@rollup/plugin-node-resolve');
 const pkg = require('./package.json');
 
 export default {
-  input: 'src/main.js',
+  input: 'src/index.ts',
   output: [
     {
       file: pkg.main,
       format: 'cjs',
-      exports: 'named'
-    }, 
+      exports: 'named',
+      compact: true,
+    },
     {
       file: pkg.module,
       format: 'es',
-      exports: 'named'
-    }
+      exports: 'named',
+      compact: true,
+    },
   ],
   plugins: [
     babel({ babelHelpers: 'bundled' }),
-    resolve(),
+    nodeResolve(),
     commonjs(),
     gzipPlugin(),
-    typescript()
-  ]
-}
+    typescript(),
+  ],
+};
